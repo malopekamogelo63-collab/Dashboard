7928aa27052b321ab86acc6dbfadfd6db410076e
Kamogelo Malope <malopekamogelo63-collab>
2026-06-16 23:12:36 +0200

    feat: complete initial dashboard layout shell with clean components

--- src/data/mock-stock.ts
+++ src/data/mock-stock.ts
@@ -1,7 +1,7 @@
 import { StockStatus } from '../types';
-
+{/* This will trigger a low-stock alert!*/}
 export const mockStock: StockStatus[] = [
   { rewardId: 'rw-1', currentStock: 14, minStockThreshold: 5 },
-  { rewardId: 'rw-2', currentStock: 2, minStockThreshold: 3 }, // This will trigger a low-stock alert!
+  { rewardId: 'rw-2', currentStock: 2, minStockThreshold: 3 }, 
   { rewardId: 'rw-3', currentStock: 50, minStockThreshold: 10 }
 ];
